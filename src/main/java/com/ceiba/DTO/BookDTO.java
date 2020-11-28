d51257a48b0d93500e74a49775f61188b0cd1318
njaramillos <nicojara120720@gmail.com>
2020-11-28 14:31:04 -0500

    se modificaron los DTO

--- src/main/java/com/ceiba/DTO/BookDTO.java
+++ src/main/java/com/ceiba/DTO/BookDTO.java
@@ -15,6 +15,7 @@ import lombok.*;
 @ToString
 public class BookDTO {
     private Long id;
+    private String isbn;
     private String name;
     private Long numberBooks;
     private Long numberLoans;
