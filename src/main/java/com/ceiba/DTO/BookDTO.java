092860ba10b040105a09eb620529d683e20e0dea
Santiago Cardozo Q <scardozo@sumset.com>
2020-11-28 13:43:55 -0500

    Implementacion de modelMaper, ajuste en etiquetas lombok

--- src/main/java/com/ceiba/DTO/BookDTO.java
+++ src/main/java/com/ceiba/DTO/BookDTO.java
@@ -1,18 +1,22 @@
 package com.ceiba.DTO;
 
-
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
 
 /**
  * Pojo para la gestion de un libro
  *
  * @author Santiago Cardozo Q
  */
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @Data
 @ToString
-public class BookDTO {
+@Builder
+@AllArgsConstructor
 
+public class BookDTO {
+	
+	
+	
 }
