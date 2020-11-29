845fd54b89a5defb2ab8784dd996d21f6975db8e
juanchoy90 <juanda.goomez@gmail.com>
2020-11-29 10:21:26 -0500

    Create webConfig
    webconfig for cors origin

---
+++ src/main/java/com/ceiba/biblioteca/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.ceiba.biblioteca.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+@EnableWebMvc
+@Configuration
+@ComponentScan
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","DELETE");
+	}
+
+}
